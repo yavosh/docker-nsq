9e0af5ef55b948cf2e59587d3f33df3d59e713c4
Yavor Shahpasov <yavosh@gmail.com>
2021-04-16 17:04:29 +0300

    Add build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker buildx build  -t yavosh/nsq:1.2.0 --platform linux/amd64,linux/arm64 --push .
+set -exu
+
+git clone --depth 1 --branch v1.2.0 git@github.com:nsqio/nsq.git
+cp Dockerfile ./nsq/
+cd ./nsq/
+docker buildx create --use
+docker buildx build  -t eu.gcr.io/ravelin-builds/yavosh/nsq:1.2.0 --platform linux/amd64,linux/arm64 --push .
